d52c945f912a5c6db680f5afed2b06ab41299672
Dustin Speer <dspeer@outlook.com>
2019-02-21 06:50:36 -0600

    sort by checkin time on segment page

--- src/components/Segment.vue
+++ src/components/Segment.vue
@@ -7,9 +7,9 @@
         {{ segment.fromCheckpoint.name }} ({{ segment.totalDistance - segment.distance}} miles) to {{ segment.toCheckpoint.name }} ({{ segment.totalDistance }} miles)
       </div>
       <div style="margin-top: 20px;">
-        <b-table responsive striped :fields="fields" :items="leaders" style="width: 100%">
+        <b-table responsive striped :fields="fields" :items="rowData" style="width: 100%">
           <template slot="participant.fullName" slot-scope="data">
-            <router-link :to="{ name: 'participant', params: { id: data.item.participant.bib }}">{{ data.item.participant.fullName }}</router-link>
+            <router-link :to="{ name: 'participant', params: { id: data.item.participant.bib }}">{{ data.item.participant.fullName }} - #{{ data.item.participant.bib }}</router-link>
           </template>
         </b-table>
       </div>
@@ -23,15 +23,94 @@ import { State, Action, Getter } from 'vuex-class';
 import moment from 'moment';
 
 @Component
-export default class Race extends Vue {
+export default class Segment extends Vue {
   @Getter('getSegmentById') private getSegmentById: any;
   @Getter('getRaceById') private getRaceById: any;
   @Getter('getLeadersByRaceId') private getLeadersByRaceId: any;
 
   private fields: any = [
+    { label: 'Place', key: 'place' },
     { label: 'Name', key: 'participant.fullName' },
     { label: 'When', key: 'when', formatter: 'timeFormatter' },
+    { label: 'Time', key: 'segmentTime'},
+    { label: 'Pace', key: 'segmentPace'},
   ];
+
+  get rowData() {
+    const out: any = [];
+    let place: number = 1;
+    let previous = null;
+    const thisSegment = this.segment;
+
+    if (this.segment) {
+      this.leaders.forEach((leader: any) => {
+        if (thisSegment.order === 1) {
+          previous = moment(this.race.start);
+        } else {
+          previous = moment(leader.checkins[thisSegment.order - 1]);
+        }
+
+        leader.place = place;
+        leader.thisCheckin = leader.checkins[thisSegment.order];
+        leader.segmentCheckinTime = leader.checkins[thisSegment.order] ?
+          moment(leader.checkins[thisSegment.order]).format('ddd, LT') : '';
+        leader.segmentTime = leader.checkins[thisSegment.order] ?
+          this.timeDifference(previous, moment(leader.checkins[thisSegment.order])) : '';
+        leader.segmentPace = this.calculatePace(
+          moment.duration(moment(leader.checkins[thisSegment.order]).diff(previous)).asSeconds(),
+          thisSegment.distance);
+
+        out.push(leader);
+        place++;
+      });
+    }
+
+    out.sort((first: any, second: any) => {
+      const firstTime = moment(first.thisCheckin);
+      const secondTime = moment(second.thisCheckin);
+      
+      if (firstTime.isBefore(secondTime)) {
+        return -1;
+      }
+
+      if (firstTime.isAfter(secondTime)) {
+        return 1;
+      }
+
+      return 0;
+    });
+
+    return out;
+  }
+
+  private timeDifference(from: any, to: any) {
+    return this.formatTimeFromSeconds(moment.duration(to.diff(from)).asSeconds());
+  }
+
+  private calculatePace(seconds: number, distance: number) {
+    return this.formatTimeFromSeconds(Math.floor(seconds / distance));
+  }
+
+  private formatTimeFromSeconds(totalSeconds: number) {
+    const hours = totalSeconds / (60 * 60);
+    const absoluteHours = Math.floor(hours);
+    const h = absoluteHours > 9 ? absoluteHours : '0' + absoluteHours;
+
+    const minutes = (hours - absoluteHours) * 60;
+    const absoluteMinutes = Math.floor(minutes);
+    const m = absoluteMinutes > 9 ? absoluteMinutes : '0' +  absoluteMinutes;
+
+    const seconds = (minutes - absoluteMinutes) * 60;
+    const absoluteSeconds = Math.floor(seconds);
+    const s = absoluteSeconds > 9 ? absoluteSeconds : '0' + absoluteSeconds;
+
+    if (absoluteHours > 0) {
+      return absoluteHours + ':' + m + ':' + s;
+    } else {
+      return absoluteMinutes + ':' + s;
+    }
+
+  }
 
   private participantLinkFormatter(value: any, key: any, item: any) {
     return '<a href="/participant/' + item.participant.bib + '">' + item.participant.fullName + '</a>';
